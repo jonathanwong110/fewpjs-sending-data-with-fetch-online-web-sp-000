d6b9fbfdee69146f1ee65ac4968a5267d6da885e
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-07 04:50:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
 // Add your code here
 
-function submitData(destinationURL, configurationObject) {
-}
-fetch('http://localhost:3000/users', {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify({
-    dogName: "Byron",
-    dogBreed: "Poodle"
-  })
-});
