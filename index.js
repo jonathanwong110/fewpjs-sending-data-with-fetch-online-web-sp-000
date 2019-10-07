d306faa5396c918c5ee498e10c7054c228c1cfa5
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-07 04:06:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // Add your code here
 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
 function submitData() {
   fetch("http://localhost:3000/users", configObj)
   .then(function(response) {
