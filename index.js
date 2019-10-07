296ea277aa27a5316b211603802892afe8790bd9
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-07 03:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 // Add your code here
 
 function submitData() {
-  return fetch("http://localhost:3000/")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(json){
-      renderBooks(json);
-    });
+
 }
