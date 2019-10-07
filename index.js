695796d1dc94df70b0cafd1d613baf866723f770
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-07 04:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 // Add your code here
 
-function submitData("http://localhost:3000/users", ) {
-  fetch("http://localhost:3000/users", configObj)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  });
+function submitData("http://localhost:3000/users", configurationObject) {
 }
