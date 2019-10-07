701e3bd9425138cf5f0f68b7705337344a3c75f1
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-07 04:51:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Add your code here
 
+let formData = {
+  name: "Steve",
+  email: "steve@steve.com"
+};
+ 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
