2c75825ec80c8ffb69ff52f3b5055239e7d65532
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-07 03:54:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add your code here
 
 function submitData() {
-  return fetch('https://anapioficeandfire.com/api/books')
+  return fetch('http://localhost:3000'')
     .then(function(response) {
       return response.json();
     })
