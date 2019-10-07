8ca9581585ec13622dfdcb1d80152ffbf7a4bdda
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-07 05:08:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,14 @@ let configObj = {
 };
 
 function submitData(name, email) {
-  return fetch("http://localhost:3000/dogs", configObj)
+  return fetch("http://localhost:3000/users", configObj)
   .then(function(response) {
     return response.json();
   })
   .then(function(object) {
-    console.log(object);
-  });
+    document.body.innerHTML = object[ "id" ]
+  })
+  .catch(function(error) {
+    document.body.innerHTML = error.message
+  })
 }
